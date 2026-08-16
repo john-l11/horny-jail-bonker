fcbbcc0330f24ce86c66ef7686cf7b3c5c29d9fe
john-l11 <john.e.lazaro@gmail.com>
2026-08-16 11:14:23 -0600

    initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+/**
+ * This is the entrypoint for the microcontroller
+ * @file main.c
+ * @author johne
+ * @date 2026-08-16
+ * @brief Main function
+ */
+int main(){
+
+    // Add your code here and press Ctrl + Shift + B to build
+    while(1) {
+        
+    }
+
+    return 0;
+}
