f90275b5a73e00ebfe9070f49a4ec76667720fda
John <john.e.lazaro@gmail.com>
2026-08-17 18:21:58 -0600

    made the interrupts file and stubbed out the bonk function for the motor

---
+++ src/interrupts.c
@@ -0,0 +1,11 @@
+/**
+ * @file interrupts.c
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2026-08-17
+ *
+ * @copyright Copyright (c) 2026
+ * This file will be used for holding all the interrupts of the microcontroller
+ * which will most likely be triggered through the instagram webhook
+ */
--- src/pwm.c
+++ src/pwm.c
@@ -1,5 +1,12 @@
 #include "../headers/includes.h"
 
-void pwmInit()
-{
-}
+void pwmInit() {}
+
+/**
+ * @brief
+ * This function will be used once a dm is detected by mr horny himself.
+ * it will alter the width of the pulse to make sure that the motor moves
+ * the fly swatter to smack john on the head and then it will close the circuit
+ * of the swatter to trigger the shock.
+ */
+void bonk() {}
